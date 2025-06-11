26202bec94c11a4c6bfcf01cd3ad38905915b5c9
Umair Imran <umairva7@gmail.com>
2025-06-12 00:33:50 +0500

    Added Login Feature

--- profiles/urls.py
+++ profiles/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import views
+from .views import UserLoginApiView
 
 router = DefaultRouter()
 router.register('hello-viewset', views.HelloViewSet, base_name='hello-viewset')
 router.register('profile', views.UserProfileViewSet)
 
 urlpatterns = [
-    path('hello/', views.HelloAPIView.as_view()),  # For APIView
+    path('hello/', views.HelloAPIView.as_view()),# For APIView
+    path('login/', UserLoginApiView.as_view()),
+
     path('', include(router.urls)),                # For ViewSet
 ]
--- profiles/views.py
+++ profiles/views.py
@@ -5,7 +5,8 @@ from rest_framework import status,viewsets,filters
 from profiles import serializers, models
 from rest_framework.authentication import TokenAuthentication
 from profiles import permissions
-
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.settings import api_settings
 class HelloAPIView(APIView):
     """Test API View"""
 
@@ -89,3 +90,7 @@ class UserProfileViewSet(viewsets.ModelViewSet):
     permission_classes = (permissions.UpdateOwnProfile,)
     filter_backends = (filters.SearchFilter,)
     search_fields = ('name', 'email',)
+
+class UserLoginApiView(ObtainAuthToken):
+    """Handle creating user authentication tokens"""
+    renderer_classes = api_settings.DEFAULT_RENDERER_CLASSES
